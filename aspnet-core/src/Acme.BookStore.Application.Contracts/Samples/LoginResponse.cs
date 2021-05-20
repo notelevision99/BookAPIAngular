e48603285c1b6442a56b02c0c8a2c66b3ccabfd1
TPSSOFT\son.le <notelevision99@gmail.com>
2021-05-20 10:32:01 +0700

    HandleUI Authenticate

---
+++ aspnet-core/src/Acme.BookStore.Application.Contracts/Samples/LoginResponse.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Acme.BookStore.Samples
+{
+    public class LoginResponse
+    {
+        public string Token { get; set; }
+        public DateTime Expiration { get; set; }
+        public string UserName { get; set; }
+        public string Email { get; set; }
+    }
+}
---
+++ aspnet-core/src/Acme.BookStore.Application/BookStoreApplicationModule.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using Volo.Abp.Account;
+using Volo.Abp.Autofac;
+using Volo.Abp.AutoMapper;
+using Volo.Abp.FeatureManagement;
+using Volo.Abp.Identity;
+using Volo.Abp.Modularity;
+using Volo.Abp.PermissionManagement;
+using Volo.Abp.SettingManagement;
+using Volo.Abp.TenantManagement;
+
+namespace Acme.BookStore
+{
+    [DependsOn(
+        typeof(BookStoreDomainModule),
+        typeof(AbpAccountApplicationModule),
+        typeof(BookStoreApplicationContractsModule),
+        typeof(AbpIdentityApplicationModule),
+        typeof(AbpPermissionManagementApplicationModule),
+        typeof(AbpTenantManagementApplicationModule),
+        typeof(AbpFeatureManagementApplicationModule),
+        typeof(AbpSettingManagementApplicationModule),
+        typeof(AbpAutoMapperModule),
+        typeof(AbpAutofacModule)
+        )]
+    public class BookStoreApplicationModule : AbpModule
+    {
+        public override void ConfigureServices(ServiceConfigurationContext context)
+        {
+            context.Services.AddAutoMapperObjectMapper<BookStoreApplicationModule>();
+            Configure<AbpAutoMapperOptions>(options =>
+            {
+                options.AddMaps<BookStoreApplicationModule>();
+            });
+        }
+    }
+}
